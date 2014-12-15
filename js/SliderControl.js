66e68a4d3e0a77fb45fbc820f49ee9b1be6c6e35
Alejandro <aaschwedhelm@gmail.com>
2014-12-15 11:15:04 -0500

    final commit

---
+++ js/SliderControl.js
@@ -0,0 +1,126 @@
+L.Control.SliderControl = L.Control.extend({
+    options: {
+        position: 'topright',
+        layers: null,
+        maxValue: -1,
+        minValue: -1,
+        markers: null,
+        range: false,
+        activeLayers: [],
+        follow: false
+    },
+
+    initialize: function (options) {
+        L.Util.setOptions(this, options);
+        this._layer = this.options.layer;
+
+    },
+
+    setPosition: function (position) {
+        var map = this._map;
+
+        if (map) {
+            map.removeControl(this);
+        }
+
+        this.options.position = position;
+
+        if (map) {
+            map.addControl(this);
+        }
+        this.startSlider();
+        return this;
+    },
+
+    onAdd: function (map) {
+        this.options.map = map;
+
+        // Create a control sliderContainer with a jquery ui slider
+        var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
+        $(sliderContainer).append('<div id="leaflet-slider" style="width:300px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:300px; margin-top:10px;"></div></div>');
+        //Prevent map panning/zooming while using the slider
+        $(sliderContainer).mousedown(function () {
+            map.dragging.disable();
+        });
+        $(document).mouseup(function () {
+            map.dragging.enable();
+            //Only show the slider timestamp while using the slider
+            $('#slider-timestamp').html('');
+        });
+
+        var options = this.options;
+        this.options.markers = [];
+
+        //If a layer has been provided: calculate the min and max values for the slider
+        if (this._layer) {
+            this._layer.eachLayer(function (layer) {
+                if (options.minValue === -1) {
+                    options.minValue = layer._leaflet_id;
+                }
+                options.maxValue = layer._leaflet_id;
+                options.markers[layer._leaflet_id] = layer;
+            });
+            this.options = options;
+        } else {
+            console.log("Error: You have to specify a layer via new SliderControl({layer: your_layer});");
+        }
+        return sliderContainer;
+    },
+
+    onRemove: function (map) {
+        //Delete all markers which where added via the slider and remove the slider div
+        for (i = this.options.minValue; i < this.options.maxValue; i++) {
+            map.removeLayer(this.options.markers[i]);
+        }
+        $('#leaflet-slider').remove();
+    },
+
+    startSlider: function () {
+        var breakPoints = [25, 1750, 8618, 18162, 27031, 27786, 29803];
+        _options = this.options;
+        $("#leaflet-slider").slider({
+            range: _options.range,
+            value: _options.minValue + 1,
+            min: _options.minValue,
+            max: breakPoints[6],
+            step: 1,
+            slide: function (e, ui) {
+                var map = _options.map;
+
+                for (var i = 0; i < breakPoints.length; i++) {
+                    if (ui.value >= breakPoints[i]) {
+                        var layerIsActive = false;
+                        for (var j = 0; j < _options.activeLayers.length; j++) {
+                            if (breakPoints[i] === _options.activeLayers[j]) {
+                                layerIsActive = true;
+                            }
+                        }
+                        if (!layerIsActive && breakPoints[i] !== breakPoints[6]) {
+                           _options.map.addLayer(_options.markers[breakPoints[i]]);
+                           _options.activeLayers.push(breakPoints[i]);
+                        }
+                        
+                        if (breakPoints[i] !== breakPoints[6]) {
+                            $('#slider-timestamp').html(_options.markers[breakPoints[i]].options.time.substr(0, 19));
+                        }
+                    } else {
+                        if (ui.value < breakPoints[i] && i !== 6) {
+                            _options.map.removeLayer(_options.markers[breakPoints[i]]);
+                            for (var k = 0; k < _options.activeLayers.length; k++) {
+                                if (breakPoints[i] === _options.activeLayers[k]) {
+                                    _options.activeLayers.splice(k, 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        _options.map.addLayer(_options.markers[_options.minValue]);
+        _options.activeLayers.push(_options.minValue);
+    }
+});
+
+L.control.sliderControl = function (options) {
+    return new L.Control.SliderControl(options);
+};
---
+++ js/script.js
@@ -0,0 +1,60 @@
+var sliderControl = null
+
+var myGeoJSONLayers = [];
+
+var map = L.map('map').setView([11.19059, -74.20099], 13);
+var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+        minZoom: 0,
+        maxZoom: 20
+        //All polygons within 1 Layer: [geojsonLayer]
+    }).addTo(map);
+
+
+var numberOfJSONFiles = 6;
+
+function getJSON (url, rgb, time) {
+    $.getJSON(url, function(data) {
+        // window.data = data;
+        var geojsonLayer = L.geoJson(data.features, 
+        {
+            style: {
+                fillColor: rgb,
+                fillOpacity: ".6",
+                color: "rgba(0,255,0,0.5)",
+                weight: "0",
+                opacity: ".6"
+            },
+            time: time
+        });
+        myGeoJSONLayers.push(geojsonLayer);
+        receivedData();
+    });
+}
+
+// Fetch all the GeoJSON files
+getJSON('./js/1989.geojson', 'rgba(255,0,0,.6)', 'Year: 1989');
+getJSON('./js/2000.geojson', 'rgba(255,128,0,.6)', 'Year: 2000');
+getJSON('./js/2013.geojson', 'rgba(255,255,0,.6)', 'Year: 2013');
+getJSON('./js/2026.geojson', 'rgba(128,255,0,.6)', 'Year: 2026');
+getJSON('./js/2039.geojson', 'rgba(0,255,255,.6)', 'Year: 2039');
+getJSON('./js/2078.geojson', 'rgba(0,0,255,.6)', 'Year: 2078');
+
+
+function receivedData () {
+    if (myGeoJSONLayers.length !== numberOfJSONFiles) {
+        return;
+    }
+    var layerGroup = L.layerGroup(myGeoJSONLayers);
+    var sliderControl = L.control.sliderControl({position: "topright", layer:layerGroup});
+    //Make sure to add the slider to the map
+    map.addControl(sliderControl);
+    //And initialize the slider
+    sliderControl.startSlider();
+}
+
+
+
+
+
+
